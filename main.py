754e67dd3fd45f1d9cdfdbc28d20ae943a76c662
jhorn00 <dawshorn@gmail.com>
2021-08-16 20:18:02 -0400

    solving of 9x9 puzzles now possible

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from solver import validate
+from solver import *
 testBoard = [[9, 0, 0, 6, 4, 0, 0, 0, 3],
              [2, 7, 0, 0, 9, 0, 5, 8, 0],
              [0, 1, 0, 5, 8, 0, 0, 0, 0],
@@ -9,3 +9,12 @@ testBoard = [[9, 0, 0, 6, 4, 0, 0, 0, 3],
              [0, 5, 1, 0, 7, 0, 0, 2, 8],
              [4, 0, 0, 0, 1, 6, 0, 0, 5]]
 print(validate(testBoard))
+for i in range(0, len(testBoard)):
+        print(testBoard[i])
+print()
+print()
+ans = solve(testBoard, 0, 0)
+print()
+for i in range(0, len(testBoard)):
+        print(testBoard[i])
+
--- solver.py
+++ solver.py
@@ -1,5 +1,7 @@
+# This function validates the board and returns the result of the analysis.
+# Efficiency may be revised later, I am aware there are optimizations to be made.
+# Currently it should support the validation of any sudoku board size, assuming only ints are used.
 def validate(board):
-    # first thing we need to do is validate the board
     # validate rows:
     for i in board:
         dup_set = set()
@@ -9,7 +11,7 @@ def validate(board):
                 dup_set.add(j)
                 compare.append(j)
         if len(dup_set) != len(compare):
-            return "invalid board"
+            return False
     # validate columns:
     for x in range(0, len(board)):
         dup_set = set()
@@ -19,7 +21,7 @@ def validate(board):
                 dup_set.add(i[x])
                 compare.append(i[x])
         if len(dup_set) != len(compare):
-            return "invalid board"
+            return False
     # validate grids:
     # calculate how many grids there are
     num_grids = int((len(board) / 3) * (len(board) / 3))
@@ -39,5 +41,49 @@ def validate(board):
                 dup_set.add(j)
                 compare.append(j)
         if len(dup_set) != len(compare):
-            return "invalid board"
-    return "valid board"
+            return False
+    return True
+
+
+# Possible error for larger boards here, but we will not allow for those.
+def check(board, row, col, num):
+    for x in range(0, len(board)):
+        if board[row][x] == num:
+            return False
+    for x in range(0, len(board)):
+        if board[x][col] == num:
+            return False
+    startY = row - row % 3
+    startX = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + startY][j + startX] == num:
+                return False
+    return True
+
+
+# Solves a valid board using backtracking, returns true when finished.
+# I opted to use the detected board dimensions rather than hard-code it
+# because I *might* add larger board solving later. It appears there is no standard
+# implementation or rules for 16x16 and larger boards. The problem this causes for me
+# is implementing solutions for integer and character puzzle types. I feel it provides
+# negligible educational benefit to go through the trouble of adding such a feature.
+def solve(board, row, col):
+    # if we reach the end return
+    if row == len(board) - 1 and col == len(board):
+        print("finishes")
+        return True
+    # move to next row when necessary
+    if col == len(board):
+        row += 1
+        col = 0
+    # skip non-empty cells
+    if board[row][col] != 0:
+        return solve(board, row, col + 1)
+    for num in range(1, len(board) + 1):
+        if check(board, row, col, num):
+            board[row][col] = num
+            if solve(board, row, col + 1):
+                return True
+        board[row][col] = 0
+    return False
