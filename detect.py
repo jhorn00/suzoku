c6c5ad76e5af3151135f9ba8689f710faf800fa9
jhorn00 <dawshorn@gmail.com>
2021-08-17 20:11:15 -0400

    Initial board detection groundwork

---
+++ detect.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+def contours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for i in contours:
+        area = cv2.contourArea(i)
+        print(area)
+        if area > 5000:
+            cv2.drawContours(imgContour, i, -1, (255, 0, 0), 3)
+            perimeter = cv2.arcLength(i, True)
+            approximation = cv2.approxPolyDP(i, 0.02 * perimeter, True)
+            print(approximation)
+            x, y, w, h = cv2.boundingRect(approximation)
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+            cropped = imgCopy[y:(y+h), x:(x+w)]
+            cv2.imshow("Cropped Board", cropped)
+
+
+vid = cv2.VideoCapture(0)
+while(True):
+    ret, img = vid.read()
+    imgContour = img.copy()
+    imgCopy = img.copy()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 50, 50)
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    closing = cv2.morphologyEx(imgCanny, cv2.MORPH_CLOSE, kernel)
+    contours(closing)
+    cv2.imshow("Input", img)
+    cv2.imshow("Cont", imgContour)
+    cv2.waitKey(1)
+
+# path = 'test_images/testbrd2.png'
+# img = cv2.imread(path)
+# imgContour = img.copy()
+# imgCopy = img.copy()
+#
+# imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+# imgCanny = cv2.Canny(imgBlur, 50, 50)
+# kernel = np.ones((5, 5))
+# imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+# closing = cv2.morphologyEx(imgCanny, cv2.MORPH_CLOSE, kernel)
+# contours(closing)
+# cv2.imshow("Input", img)
+# cv2.imshow("Gray", imgGray)
+# cv2.imshow("Blur", imgBlur)
+# cv2.imshow("Canny", imgCanny)
+# cv2.imshow("Dilated", imgCanny)
+# cv2.imshow("Cont", imgContour)
+# cv2.imshow("Close", closing)
+# cv2.waitKey(0)
